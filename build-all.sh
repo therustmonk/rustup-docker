f2f0a07debf82c43af3886bbde3929148daf07f1
Denis Kolodin <deniskolodin@gmail.com>
2017-08-13 12:05:41 +0300

    Add Amazon Linux Image of EC2

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,4 @@
-docker build -t rustup/debian:jessie ./debian-jessie
-docker build -t rustup/centos:7 ./centos-7
-docker build -t rustup/cedar:14 ./cedar-14
+docker build -t 'rustup/debian:jessie' './debian-jessie'
+docker build -t 'rustup/centos:7' './centos-7'
+docker build -t 'rustup/cedar:14' './cedar-14'
+docker build -t 'rustup/amazonlinux:2017.03' './amazonlinux-2017.03'
